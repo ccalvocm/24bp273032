9ffe8170f41623826e2f2526716922625eb0d0f0
Carlos Calvo Cortés-Monroy <52244870+ccalvocm@users.noreply.github.com>
2025-04-15 21:48:02 -0400

    Update ecmwf_nium.py

--- src/ecmwf_nium.py
+++ src/ecmwf_nium.py
@@ -2,19 +2,17 @@ from ecmwf.opendata import Client
 import xarray as xr
 from datetime import date, timedelta
 
-days_back = 20
-target_date = date.today() - timedelta(days=days_back)
+target_date = date.today()
 day = target_date.strftime("%Y%m%d")
-run = "00"  # or "12", "06", "18" depending on what's available
-
+step=[12,24]
 client = Client(source="ecmwf")
 
 request = {
-"date": -3,
-"time": 12,
+"date": day,
+"time": 0,
 "type": "fc",
-"step": [3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60],
-"param": [ "tp"],
+"step": step,
+"param": ["tp"],
 }
 
 ds=client.retrieve(request, "surface1.grib2")
