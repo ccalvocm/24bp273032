15fd5b7b8d6b0f824752260986f3c0d5ee61264c
Carlos Calvo Cortés-Monroy <52244870+ccalvocm@users.noreply.github.com>
2025-06-13 17:16:41 -0400

    add: working example

--- src/hydrostreamer.r
+++ src/hydrostreamer.r
@@ -1,4 +1,5 @@
 install.packages("devtools")
+install.packages("lwgeom")
 devtools::install_github("mkkallio/hydrostreamer")
 
 library(hydrostreamer)
@@ -7,31 +8,60 @@ library(lubridate)
 library(dplyr)
 library(sf)
 
-data(example_rivers)
+library(sf)
+library(lwgeom)    # for st_make_valid()
+
 data(example_basins)
+data(example_rivers)
+
 runoff <- brick(system.file("extdata", "runoff.tif", package = "hydrostreamer"))
 dem <- brick(system.file("extdata", "dem.tif", package = "hydrostreamer")) 
-plot(runoff[[1]]) 
-plot(basins)
-plot(st_union(basins), add=TRUE)
-plot(river, add=TRUE)
+# plot(runoff[[3]]) 
+# plot(basins)
+# plot(st_union(basins), add=TRUE)
+# plot(river, add=TRUE)
+# plot(dem, add=TRUE)
+
+
+# DASYMETRIC MAPPING WITH LINES
+# Try method 1: force simple geometries
+tryCatch({
+  basins <- st_cast(basins, "MULTIPOLYGON")
+  basins <- st_buffer(basins, 0)
+  basins <- sf::st_make_valid(basins)
+}, error=function(e) message("Method 1 failed:", e$message))
+
+# Method 2: Remove problematic basins one by one
+valid_basins <- st_is_valid(basins, reason=TRUE)
+problem_basins <- which(valid_basins != "Valid Geometry")
+if(length(problem_basins) > 0) {
+  message(paste0("Removing ", length(problem_basins), " problematic basins."))
+  basins <- basins[-problem_basins,]
+}
+
+# Manually bypass st_union which is causing problems
+message("Creating simplified AOI...")
+aoi <- st_as_sfc(st_bbox(basins))
+
+runoff <- brick(system.file("extdata","runoff.tif", package="hydrostreamer"))
+dem    <- brick(system.file("extdata","dem.tif",    package="hydrostreamer"))
+
+# Use bounding box instead of union
 source_runoff <- raster_to_HS(runoff, 
-                              unit = "mm/s",
-                              date = ymd("1980-01-01"), 
-                              timestep = "month", 
-                              aoi = st_union(basins),
-                              names = "LORA")
+                             unit = "mm/s",
+                             date = ymd("1980-01-01"), 
+                             timestep = "month", 
+                             aoi = aoi,  # Using bounding box as AOI
+                             names = "LORA")
 
 elevation_values <- raster::extract(dem, basins)
 basins$elevation <- sapply(elevation_values, mean)
 river <- dplyr::filter(river, SEGMENT_ID %in% basins$SEGMENT_ID) %>% 
   dplyr::mutate(elevation = basins$elevation)
-A2LDM <- interpolate_runoff(source_runoff, river,
-                            dasymetric = "elevation",
-                            riverID = "SEGMENT_ID")
-#> although coordinates are longitude/latitude, st_intersects assumes that they are planar
+RB <- interpolate_runoff(source_runoff,
+                         river,
+                         basins = basins,
+                         riverID = "SEGMENT_ID")
 
-A2LDM$mean_runoff <- sapply(A2LDM$runoff_ts, function(x) mean(x$LORA))
-plot(A2LDM[,"mean_runoff"])
-
+plot(RB)
 
